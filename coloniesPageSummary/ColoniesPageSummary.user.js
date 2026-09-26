33bd1c5e82aee335d72ea408e31f9ec10eea2c73
warppooted <nicholasjhwee@mgmail.com>
2026-09-26 18:39:45 +0800

    Still WIP. Colony names inserted to table.

--- coloniesPageSummary/ColoniesPageSummary.user.js
+++ coloniesPageSummary/ColoniesPageSummary.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AtmoBurn - Colonies Page Summary
 // @namespace    CavalryMaid.AtmoBurn.ColoniesPage
-// @version      v0.0.2
+// @version      v0.0.3
 // @description  Big page, many data. Sort sort.
 // @author       CavalryMaid
 // @match        https://*.atmoburn.com/overview.php?view=1
@@ -15,10 +15,14 @@
     const summaryTable = document.createElement('div');
 
     // Table specs - headers
-    const tableHeaders = ['Colony', 'Population', 'Wealth', 'Surplus', 'Happiness', 'Education', 'Crime', 'Unemployment'];
+    const tableHeaders = ['Colony', 'Population', 'Wealth', 'Surplus', 'Food', 'Happiness', 'Education', 'Crime', 'Unemployment'];
 
     // Table initialization and styles
     summaryTable.innerHTML = `
+        <style>
+            #colonies-overview-table { border-collapse: collapse; box-shadow: 0 0 10px rgba(0,0,0,0.3); table-layout: fixed; width: 100%; }
+            #colonies-overview-table th, #colonies-overview-table td { background-color: #3d3d3d; color: white; border: 1px solid #ccc; padding: 6.5px; font-size: 10.5px;  }                     
+        </style>
         <table id='colonies-overview-table'>
             <thead id='colonies-overview-table-header'>
                 <tr></tr>
@@ -40,5 +44,32 @@
         const newHeader = targetHeaderRow.insertCell();
         newHeader.textContent = headerText;
     };
+
+    // Get array of colonies shown and append each as a table row - WIP    
+    const coloniesInContainer = document.querySelector('#coloniesContainer');
+    const allLinks = coloniesInContainer.querySelectorAll('a[href*=view_colony]');
+    const visibleColonyArray = [];
+    allLinks.forEach(link => {
+        if (link.offsetParent !== null){
+            visibleColonyArray.push(link);
+        }
+    });
+         
+    console.log(visibleColonyArray);  // returns array of divs with #id of visible colonies
+    
+    for (let i=0; i<visibleColonyArray.length; i++){
+        const colonyLink = visibleColonyArray[i].outerHTML;        
+                
+        const tableRow = `
+          <tr>          
+          <td>          
+          <span style="font-size: 13px; font-weight: 600;">${colonyLink}</span>          
+          </td>
+          </tr>          
+          `
+                
+        const targetTB = document.querySelector('#colonies-overview-table-body');
+        targetTB.insertAdjacentHTML('beforeend', tableRow);
+    } 
 
 })();
